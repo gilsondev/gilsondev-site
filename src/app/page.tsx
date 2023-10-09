82c2826d51b2bfb5b8f0f22fa81f79ccbdf081d6
Gilson Filho <me@gilsondev.in>
2023-10-09 14:26:43 -0300

    feat: last work experiences

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutMe from "@/components/aboutme";
 import BlogPosts from "@/components/blogposts";
+import Experience from "@/components/experience";
 import Header from "@/components/header";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <main className="flex flex-col gap-4">
         <AboutMe />
+        <Experience />
         <BlogPosts />
       </main>
     </div>
---
+++ src/components/experience/index.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import React from "react";
+
+const lastExperiences = [
+  {
+    organizationImage: "/jusbrasil.jpg",
+    title: "Senior Fullstack Software Engineer - Jusbrasil",
+    date: "2021 - Atual",
+  },
+  {
+    organizationImage: "/behup.jpg",
+    title: "Java Software Engineer - Behup",
+    date: "2019",
+  },
+  {
+    organizationImage: "/capgemini.jpg",
+    title: "Data Engineer - Capgemini",
+    date: "2019",
+  },
+  {
+    organizationImage: "/enap.jpg",
+    title: "Senior Python Developer - Enap",
+    date: "2017",
+  },
+];
+
+const Experience = () => {
+  return (
+    <div className="mt-16">
+      <h3 className="text-xl font-semibold dark:text-gray-200">
+        Últimas experiências
+      </h3>
+      <ul className="list-none flex flex-col gap-5 mt-3">
+        {lastExperiences.map((experience, index) => (
+          <li key={index} className="flex items-center gap-1">
+            <Image
+              src={experience.organizationImage}
+              width={64}
+              height={64}
+              alt="Logo Jusbrasil"
+              objectFit="cover"
+              className="rounded-full"
+            />
+            <div className="flex flex-col gap-1 items-start">
+              <h4 className="px-5 font-semibold">{experience.title}</h4>
+              <span className="pl-5 text-xs">{experience.date}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Experience;
